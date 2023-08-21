2258931a723efa6fc6e647d48a973b526d390a8e
Georgi <georgi.rosenov17@gmail.com>
2023-08-21 21:11:39 +0300

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './components/button/button.component';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-is';
+
+  test() {
+    console.log('test');
+  }
+
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
 }
