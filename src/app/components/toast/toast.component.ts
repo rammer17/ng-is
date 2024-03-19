08b73488e206d724df111f4a030c82d8032a7caa
Georgi <96887145+rammer17@users.noreply.github.com>
2024-03-19 09:48:00 +0300

    Started working on toast animation

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -1,4 +1,5 @@
-import { Component, InputSignal, input } from "@angular/core";
+import { trigger, state, style, transition, animate } from "@angular/animations";
+import { Component } from "@angular/core";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @Component({
@@ -7,9 +8,28 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
   imports: [FontAwesomeModule],
   templateUrl: "./toast.component.html",
   styleUrl: "./toast.component.scss",
+  animations: [
+    trigger("createAnimation", [
+      transition("* => true", [
+        style({ opacity: 0, transform: "translateY(200%)" }),
+        animate("250ms", style({ opacity: 1, transform: "translateY(0) scale(1)" })),
+      ]),
+      transition("* => false", [
+        style({ opacity: 1, transform: "scale(1)" }),
+        animate("250ms", style({ opacity: 0, transform: "scale(1)" })),
+      ]),
+    ]),
+    trigger("shrinkAnimationFirst", [
+      state("true", style({ transform: "scale(.75)", "transform-origin": "top", "margin-bottom": "2rem" })),
+      transition("* => true", [
+        style({ transform: "scale(1)", "transform-origin": "top" }),
+        animate("250ms", style({ transform: "scale(.75)", "margin-bottom": "2rem" })),
+      ]),
+    ]),
+  ],
 })
 export class ToastComponent {
-  position: InputSignal<any> = input("bottom-right");
-
-  toastClass(): any {}
+  toastClass(): any {
+    return {};
+  }
 }
